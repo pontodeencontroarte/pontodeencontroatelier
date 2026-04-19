8c668df7ff045a1921c9c2a09d736d660c5be2fb
pontodeencontroarte <amandaalvesfigueira@gmail.com>
2026-04-18 23:27:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function whatsapp1() {
+window.open("https://wa.me/5511916512558?text=Olá! Vim pelo site Ponto de Encontro e gostaria de mais informações 😊", "_blank");
+}
+
+function whatsapp2() {
+window.open("https://wa.me/5511976624771?text=Olá! Vim pelo site Ponto de Encontro e gostaria de mais informações 😊", "_blank");
+}
